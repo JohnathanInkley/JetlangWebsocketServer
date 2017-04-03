f805fac5d24f2ee4415eb93e3526f966b347ea4d
JohnathanInkley <jcniboc@gmail.com>
2017-04-03 21:39:34 +0100

    Trying to commit to heroku

--- src/main/java/RunServerAndLogger.java
+++ src/main/java/RunServerAndLogger.java
@@ -1,5 +1,8 @@
 import org.jetlang.channels.MemoryChannel;
 
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class RunServerAndLogger {
     public static void main(String[] args) {
         MemoryChannel<ClientMessage> clientMessageChannel = new MemoryChannel<>();
@@ -9,11 +12,15 @@ public class RunServerAndLogger {
         connectionLogger.attachInputChannelAndSubscribe(clientMessageChannel);
         connectionLogger.attachOutputChannel(loggerMessageChannel);
 
-        Server server = new Server(4444);
+        Server server = new Server(Integer.valueOf(args[0]));
         server.attachClientMessageChannel(clientMessageChannel);
         server.attachLoggerMessageChannel(loggerMessageChannel);
         server.start();
 
+        staticFileLocation("/public");
+        get("", (res, req) -> {
+            return null;
+        });
 
         while (true) {
 
